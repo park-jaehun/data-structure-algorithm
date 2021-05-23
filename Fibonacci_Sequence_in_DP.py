c9d2f3bc116b2b7843ff3c3bef86922a33a5da5d
park-jaehun <pjhun588@gmail.com>
2021-05-23 23:59:04 +0900

    피보나치 DP 구현

---
+++ Fibonacci_Sequence_in_DP.py
@@ -0,0 +1,13 @@
+# Implemetation Example
+def FibonacciDP(n):
+    # Setting up a memoization table
+    dictFiboncci = {}
+    dictFiboncci[0] = 1
+    dictFiboncci[1] = 1
+    #building up a bigger solutions
+    for itr in range(2, n + 1):
+        dictFiboncci[itr] = dictFiboncci[itr - 1] + dictFiboncci[itr - 2]
+    return dictFiboncci[n]
+
+for i in range(0, 3):
+    print("FibonacciDP print: ",FibonacciDP(i))
