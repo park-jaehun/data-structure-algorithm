1f0f2e3fcd77c54a2f57af724e2037aeb8ceba4d
park-jaehun <pjhun588@gmail.com>
2021-07-13 08:24:43 +0900

    dep 라이브러리로 큐 이해

--- Queue.py
+++ Queue.py
@@ -1,20 +1,32 @@
 # coding=<euc-kr>
-from SLS import SinglyLinkedList
-class Queue(object):
-    IsInstance = SinglyLinkedList()
-    def Enque(self,value):
-        return self.IsInstance.insertAt(value,0)
-    def Deque(self):
-        size = self.IsInstance.getSize()
-        return self.IsInstance.removeAt(size - 1)
-    def QueStatus(self):
-        return self.IsInstance.printStatus()
+from collections import deque
 
-queue = Queue()
-queue.Enque("a")
-queue.Enque("b")
-queue.Enque("c")
-queue.Enque("d")
-queue.Deque()
-queue.Deque()
-print(queue.QueStatus())
+deq = deque([2,3,4,5,6])
+print(deq)
+deq.appendleft(1)
+print(deq)
+deq.appendleft(3)
+print(deq)
+deq.popleft()
+print(deq)
+
+#
+# from SLS import SinglyLinkedList
+# class Queue(object):
+#     IsInstance = SinglyLinkedList()
+#     def Enque(self,value):
+#         return self.IsInstance.insertAt(value,0)
+#     def Deque(self):
+#         size = self.IsInstance.getSize()
+#         return self.IsInstance.removeAt(size - 1)
+#     def QueStatus(self):
+#         return self.IsInstance.printStatus()
+#
+# queue = Queue()
+# queue.Enque("a")
+# queue.Enque("b")
+# queue.Enque("c")
+# queue.Enque("d")
+# queue.Deque()
+# queue.Deque()
+# print(queue.QueStatus())
